bd9a522007025af9608922e69dd1e424d8e34ba8
thanhto24 <110890930+thanhto24@users.noreply.github.com>
2024-10-22 21:27:33 +0700

    Delete backend directory

--- backend/src/routes/email.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/email');
-
-router.post('/send-email', emailController.sendEmail);
-
-module.exports = router;
